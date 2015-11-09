03ea51ec863db352e595a01d1832af7aebd65e4a
Johannes Meyer <johannes.meyer@netways.de>
2015-11-09 13:40:42 +0100

    Add basic configuration dialog

---
+++ application/forms/Config/GeneralConfigForm.php
@@ -0,0 +1,103 @@
+<?php
+/* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Module\Generictts\Forms\Config;
+
+use Zend_Validate_Callback;
+use Icinga\Forms\ConfigForm;
+use Icinga\Web\Notification;
+
+class GeneralConfigForm extends ConfigForm
+{
+    /**
+     * Initialize this form
+     */
+    public function init()
+    {
+        $this->setName('form_config_generictts_general');
+        $this->setSubmitLabel(t('Save Changes'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createElements(array $formData)
+    {
+        $patternValidator = new Zend_Validate_Callback(function ($value) {
+            return @preg_match($value, '') !== false;
+        });
+        $patternValidator->setMessage(
+            t('"%value%" is not a valid regular expression.'),
+            Zend_Validate_Callback::INVALID_VALUE
+        );
+        $this->addElement(
+            'text',
+            'ticket_pattern',
+            array(
+                'value'         => '/this-will-not-match(\d{3-6})/',
+                'label'         => $this->translate('TTS Ticket Pattern'),
+                'description'   => $this->translate('The pattern to extract a ticket\'s id from comments and plugin output.'),
+                'requirement'   => $this->translate('The ticket pattern must be a valid regular expression.'),
+                'validators'    => array($patternValidator)
+            )
+        );
+
+        $urlValidator = new Zend_Validate_Callback(function ($value) {
+            return strpos($value, '$1') !== false;
+        });
+        $urlValidator->setMessage(
+            $this->translate('The url must contain the placeholder $1 to be substituted with a ticket\'s id.'),
+            Zend_Validate_Callback::INVALID_VALUE
+        );
+        $this->addElement(
+            'text',
+            'ticket_url',
+            array(
+                'value'         => 'http://no-such-domain.example.com/ticket?id=$1',
+                'label'         => $this->translate('TTS Ticket Url'),
+                'description'   => $this->translate('The URL to substitute with a ticket\'s id.'),
+                'requirement'   => $this->translate('Use $1 as the placeholder for the id.'),
+                'validators'    => array($urlValidator)
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onSuccess()
+    {
+        $sections = array();
+        foreach ($this->getValues() as $sectionAndPropertyName => $value) {
+            if ($value !== '') {
+                list($section, $property) = explode('_', $sectionAndPropertyName, 2);
+                $sections[$section][$property] = $value;
+            }
+        }
+
+        foreach ($sections as $section => $config) {
+            $this->config->setSection($section, $config);
+        }
+
+        if ($this->save()) {
+            Notification::success(t('New configuration has successfully been stored'));
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onRequest()
+    {
+        $values = array();
+        foreach ($this->config as $section => $properties) {
+            foreach ($properties as $name => $value) {
+                $values[$section . '_' . $name] = $value;
+            }
+        }
+
+        $this->populate($values);
+    }
+}
---
+++ configuration.php
@@ -0,0 +1,10 @@
+<?php
+/* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
+
+/** @var $this \Icinga\Application\Modules\Module */
+
+$this->provideConfigTab('general', array(
+    'title' => $this->translate('Adjust the general configuration of the GenericTTS module'),
+    'label' => $this->translate('General'),
+    'url' => 'config'
+));
