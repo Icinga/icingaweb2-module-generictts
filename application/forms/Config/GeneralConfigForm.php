5e0a175e744c42a7d64f29653007efe1b543395d
Johannes Meyer <johannes.meyer@netways.de>
2015-11-13 16:14:57 +0100

    GeneralConfigForm: Drop methods onSuccess() and onRequest()
    The base class is providing them now.

--- application/forms/Config/GeneralConfigForm.php
+++ application/forms/Config/GeneralConfigForm.php
@@ -5,7 +5,6 @@ namespace Icinga\Module\Generictts\Forms\Config;
 
 use Zend_Validate_Callback;
 use Icinga\Forms\ConfigForm;
-use Icinga\Web\Notification;
 
 class GeneralConfigForm extends ConfigForm
 {
@@ -60,44 +59,5 @@ class GeneralConfigForm extends ConfigForm
                 'validators'    => array($urlValidator)
             )
         );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function onSuccess()
-    {
-        $sections = array();
-        foreach ($this->getValues() as $sectionAndPropertyName => $value) {
-            if ($value !== '') {
-                list($section, $property) = explode('_', $sectionAndPropertyName, 2);
-                $sections[$section][$property] = $value;
-            }
-        }
-
-        foreach ($sections as $section => $config) {
-            $this->config->setSection($section, $config);
-        }
-
-        if ($this->save()) {
-            Notification::success(t('New configuration has successfully been stored'));
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function onRequest()
-    {
-        $values = array();
-        foreach ($this->config as $section => $properties) {
-            foreach ($properties as $name => $value) {
-                $values[$section . '_' . $name] = $value;
-            }
-        }
-
-        $this->populate($values);
     }
 }
